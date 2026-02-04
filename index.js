32cb13ef27f4867021290389b16550a00a1d8265
Eroh Oghenemaro Divine <maroeroh@gmail.com>
2026-02-04 20:18:12 +0100

    added dot

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     isConnected = true;
-    console.log("✅ MongoDB connected");
+    console.log("✅ MongoDB connected.");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
   }
